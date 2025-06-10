1c9be67c453148e5e15e0a6f86f556c20ea11e3e
kellerkatlin <kellerkatlin.k@gmail.com>
2025-06-10 11:50:14 -0500

    Fix formatting in access component for consistent styling

--- src/app/pages/auth/access.ts
+++ src/app/pages/auth/access.ts
@@ -13,7 +13,7 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
             <div class="flex flex-col items-center justify-center">
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, rgba(247, 149, 48, 0.4) 10%, rgba(247, 149, 48, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center" style="border-radius: 53px">
-                        <div class="gap-4 flex flex-col items-center">
+                        <div class="gap-4 flex  flex-col items-center">
                             <div class="flex justify-center items-center border-2 border-orange-500 rounded-full" style="width: 3.2rem; height: 3.2rem">
                                 <i class="text-orange-500 pi pi-fw pi-lock !text-2xl"></i>
                             </div>
